3df7d31cb721f806392d606c500b0b112a11da1c
apostle <cochranpf@vcu.edu>
2021-03-03 01:25:25 -0500

    Comments on pointer usage

--- Analyzer.c
+++ Analyzer.c
@@ -1,5 +1,17 @@
 #include "Analyzer.h"
 
+/* Thoughts on pointers:
+     -Normal variable declaration: int x = 2;
+     -Pointer declaration: int *p = &x; ('&' used for address)
+     -When using pointers later, you omit the '*' like this: p
+     -If you add the '*' back, it access the actual value at the pointer's address
+     -Arrays are different:
+        -int arr[4]; (arr is simply a pointer to the first value of the array
+        -This means that you can just pass 'arr' AS IS into functions that expect a pointer
+        -This is what the code below does, I think
+        -Also means that you must do pointer arithmetic to access other indices?
+*/
+
 int main(){
 	//initialize file pointer to NULL 
 	FILE *infile = NULL; 
